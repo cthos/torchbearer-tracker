cc9bd4b88d1c00a405388caeb93e897a364833dc
cthos <daginus@gmail.com>
2016-09-07 21:17:02 -0700

    Character migration and model.

---
+++ database/migrations/2016_09_08_040550_create_character_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCharacterTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('characters', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('stock');
+            $table->string('class');
+            $table->integer('will');
+            $table->integer('health');
+            $table->integer('nature');
+            $table->integer('max_nature');
+            $table->integer('resources');
+            $table->integer('circles');
+            $table->json('extras'); // This is where enemy, parents, etc goes.
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('characters');
+    }
+}
