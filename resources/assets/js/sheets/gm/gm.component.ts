08d8ef13d81b0544238bde95291b607beed38cc8
cthos <daginus@gmail.com>
2016-09-28 20:22:57 -0700

    Playing with ng2 data binding.

--- resources/assets/js/sheets/gm/gm.component.ts
+++ resources/assets/js/sheets/gm/gm.component.ts
@@ -6,8 +6,26 @@ import { Component, Input, Inject, OnInit } from '@angular/core';
     templateUrl: './templates/gm_sheet.html'
 })
 export class GMComponent implements OnInit {
+    phaseCount : number = 1;
+
     ngOnInit() : void {
 
+    }
+
+    decrementPhaseCounter() : void {
+        this.phaseCount--;
+
+        if (this.phaseCount <= 0) {
+            this.phaseCount = 4;
+        }
+    }
+
+    incrementPhaseCounter() : void {
+        this.phaseCount++;
+
+        if (this.phaseCount > 4) {
+            this.phaseCount = 1;
+        }
     }
 }
 
