583debf414c7c7e7dba6abd3837f131336d8c1dc
cthos <daginus@gmail.com>
2016-10-04 19:41:26 -0700

    Add town phase stuff.

--- resources/assets/js/sheets/gm/gm.component.ts
+++ resources/assets/js/sheets/gm/gm.component.ts
@@ -8,6 +8,7 @@ import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 })
 export class GMComponent implements OnInit {
     phaseCount : number = 1;
+    townPhaseId : number = 0;
     currentPhase : string = 'adventure';
     campLocation : string = 'wilderness';
     campSafety : string = 'typical';
@@ -26,6 +27,28 @@ export class GMComponent implements OnInit {
 
     rollCamp() : void {
 
+    }
+
+    getTownPhaseName() : string {
+        let phaseNames = [
+            'Spend Remaining Checks',
+            'Determine the Watch',
+            'Roll on the Entering Town table and roll again on the appropriate subtable',
+            'Dispose of Spoiled Food',
+            'Level Up',
+            'Strategize',
+            'Haggle'
+        ];
+
+        return phaseNames[this.townPhaseId];
+    }
+
+    nextTownPhase() {
+        this.townPhaseId++;
+
+        if (this.townPhaseId > 6) {
+            this.townPhaseId = 0;
+        }
     }
 
     dimTheLights() : void {
